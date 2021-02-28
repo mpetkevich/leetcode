93abe058d704d0eb1fccc33dc201e57f0d9ec3ca
Max Petkevich <PetkevichMaksim@gmail.com>
2021-03-01 02:54:46 +0300

    Time: 72 ms (94.30%), Space: 38.7 MB (56.21%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,16 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let result = [];   
-    outer: for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            if ( nums[i] + nums[j] == target) {
-                console.log (`i=${i}, j=${j} | nums[i], nums[j] = ${nums[i]} , ${nums[j]}`);
-                result = [i, j];
-                break outer;
+            if ( nums[j] == target - nums[i]) {
+                return [i, j];
             }
         }
     }
         
-    return result;
+    return [];
 };

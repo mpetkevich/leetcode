1ccca585a2e86b8dfee3ac73150cd5fd29551dd8
mpetkevich <PetkevichMaksim@gmail.com>
2021-03-04 18:31:00 +0300

    Revert "del"
    This reverts commit ed6514b9530a584d00257f7a3d33eb000460fc21.

---
+++ maximum-subarray/maximum-subarray.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// var maxSubArray = function(nums) {
+//     for (let i = 1; i < nums.length; i++){
+//         nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+//     }
+//     return Math.max(...nums);
+// };
+
+var maxSubArray = function(nums) {
+      var maxSoFar = nums[0];
+      var maxEndingHere = nums[0];
+      
+      for (var i = 1; i < nums.length; i++) {          
+        maxEndingHere = Math.max(maxEndingHere + nums[i], nums[i]);
+        maxSoFar = Math.max(maxSoFar, maxEndingHere);  
+      }
+
+      return maxSoFar;
+}

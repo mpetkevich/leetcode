a7c40dc59b557972794ea5d3eb81df25833952dd
Max Petkevich <PetkevichMaksim@gmail.com>
2023-03-02 01:19:40 +0100

    Update BF-two-sum.py

--- 0001-two-sum/BF-two-sum.py
+++ 0001-two-sum/BF-two-sum.py
@@ -1,8 +1,6 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        prevMap = {}
-        for i,n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff], i]
-            prevMap[n] = i
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i,j]
